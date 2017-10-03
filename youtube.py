25510fdd3783253887383e578145c7890d3833d5
huynq <qhuydtvt@gmail.com>
2017-10-04 02:15:06 +0700

    Fixed build error

--- youtube.py
+++ youtube.py
@@ -6,7 +6,7 @@ def get_vid_info(search_terms):
     ydl_options = {
         'format': 'bestaudio/best',
         'username': 'huynq.public',
-        'password': 'hithere1989'
+        'password': 'hithere1989',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
