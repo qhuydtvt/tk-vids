3c030b465f8f76db52a082247791a132a34ccf34
huynq <qhuydtvt@gmail.com>
2017-10-03 20:57:59 +0700

    Added audio url

--- app.py
+++ app.py
@@ -1,14 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from youtube import get_vid_info
+from flask_restful import Resource, Api
+from os.path import join
+from urllib.request import urlopen
+import json
 app = Flask(__name__)
+
+def webpage_str(url):
+    return urlopen(url).read.decode('utf-8')
+
+not_found_message = json.dumps ({
+    "sucess": 0,
+    "data": "not_found"
+})
+
+
 
 @app.route('/')
 def index():
-    return "How to use"
+    # open(join(app.root_path, "/data/music_result.txt")).read().decode('utf-8')
+    music_result = open(join(app.root_path, "data", "music_result.txt")).read()
+    guide_list = [
+        {
+            "title": "Music - Pure audio",
+            "example": "/api/music?search_terms=wake+me+up+acivii",
+            "format": "/api/music?search_terms=<Enter song|artist here>"
+        }
+    ]
+    return render_template("index.html", guide_list=guide_list)
 
 
 @app.route('/api/music')
 def music_search():
-    return "Music"
+    search_terms = request.args["search_terms"]
+    vid_info = get_vid_info(search_terms)
+    if  "entries" not in vid_info:
+        return not_found_message
+    elif len(vid_info["entries"]) == 0:
+        return not_found_message
+    else:
+        if "formats" in vid_info["entries"][0]:
+            del vid_info["entries"][0]["formats"]
+        entry = vid_info["entries"][0]
+
+        return json.dumps({
+            "sucess": 1,
+            "data": {
+                "url": entry["url"],
+                "thumbnail": entry["thumbnail"],
+                "description": entry["description"]
+            }}, indent=4)
+
 
 if __name__ == '__main__':
   app.run(debug=True)
---
+++ youtube.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+from youtube_dl import YoutubeDL
+import json
+
+def get_vid_info(search_terms):
+    ydl_options = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    }
+    with YoutubeDL(ydl_options) as ydl:
+        info = ydl.extract_info(
+                 "ytsearch1:{0}".format(search_terms),
+                 download=False)
+        return info
+
+
+if __name__ == "__main__":
+    info = get_vid_info("wake+me+up")
+    print(json.dumps(info, indent=4))
