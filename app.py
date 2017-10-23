fa61d597056c20e34b5652dae3e5cd4fb9f0e377
huynq <qhuydtvt@gmail.com>
2017-10-23 20:20:15 +0700

    .

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ def index():
     guide_list = [
         {
             "title": "Pure audio search",
-            "example": "services/api/audio?search_terms=wake+me+up+acivii",
-            "format": "services/api/audio?search_terms=<Enter song|artist here>"
+            "example": "/api/audio?search_terms=wake+me+up+acivii",
+            "format": "/api/audio?search_terms=<Enter song|artist here>"
         }
     ]
     return render_template("index.html", guide_list=guide_list)
